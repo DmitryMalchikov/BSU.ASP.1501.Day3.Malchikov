d440f4eaf6db175bddd241e036295dca4bb9f191
MalchikovDmitry <dimamal1995@gmail.com>
2015-10-22 10:52:50 +0300

    Day3 homework

---
+++ BSU.ASP.1501.Day3.Malchikov/Polynom.Tests/PolynomTests.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Polynom;
+using NUnit.Framework;
+
+namespace Polynom.Tests
+{
+    public class PolynomTests
+    {
+        static object[] addCases =
+        {
+            new object[] {new Polynom(new double[] { 1,2,3}), new Polynom(new double[] {1,2,3 }), new Polynom(new double[] {2,4,6 }) },
+            new object[] {new Polynom(new double[] { 0,2,3}), new Polynom(new double[] { 1,2,3}), new Polynom(new double[] {1,4,6 }) },
+            new object[] {new Polynom(new double[] { 0,2}), new Polynom(new double[] {1,2,3 }), new Polynom(new double[] { 1,4,3}) },
+            new object[] {new Polynom(new double[] { 0}), new Polynom(new double[] { 1,2,3}), new Polynom(new double[] {1,2,3 }) },
+            new object[] {new Polynom(new double[] { 0,0,3}), new Polynom(new double[] { 0,0,3}), new Polynom(new double[] {0,0,6 }) }
+        };
+
+        static object[] multiplyCases =
+{
+            new object[] {new Polynom(new double[] { 1,2,3}), new Polynom(new double[] {1,2,3 }), new Polynom(new double[] {1,4,10,12,9 }) },
+            new object[] {new Polynom(new double[] { 0,2,3}), new Polynom(new double[] { 1,2,3}), new Polynom(new double[] {0,2,7,12,9 }) },
+            new object[] {new Polynom(new double[] { 0,2}), new Polynom(new double[] {1,2,3 }), new Polynom(new double[] { 0,2,4,6}) },
+            new object[] {new Polynom(new double[] { 0}), new Polynom(new double[] { 1,2,3}), new Polynom(new double[] {0 }) },
+            new object[] {new Polynom(new double[] { 0,0,3}), new Polynom(new double[] { 0,0,3}), new Polynom(new double[] {0,0,0,0,9 }) },
+            new object[] {new Polynom(new double[] { }), new Polynom(new double[] { }), new Polynom(new double[] { }) }
+        };
+
+        static object[] subtractCases =
+{
+            new object[] {new Polynom(new double[] { 1,2,3}), new Polynom(new double[] {1,2,3 }), new Polynom(new double[] {0}) },
+            new object[] {new Polynom(new double[] { 0,2,3}), new Polynom(new double[] { 1,2,3}), new Polynom(new double[] {-1 }) },
+            new object[] {new Polynom(new double[] { 0,2}), new Polynom(new double[] {1,2,3 }), new Polynom(new double[] { -1,0,-3}) },
+            new object[] {new Polynom(new double[] { 0}), new Polynom(new double[] { 1,2,3}), new Polynom(new double[] {-1,-2,-3 }) },
+            new object[] {new Polynom(new double[] { 0,0,3}), new Polynom(new double[] { 0,0,3}), new Polynom(new double[] { }) },
+            new object[] {new Polynom(new double[] { }), new Polynom(new double[] { }), new Polynom(new double[] { }) }
+        };
+
+        static object[] scalarMultiplyCases =
+{
+            new object[] {new Polynom(new double[] { 1,2,3}), 1, new Polynom(new double[] {1,2,3}) },
+            new object[] {new Polynom(new double[] { 1,2,3}), 0, new Polynom(new double[] { }) },
+            new object[] {new Polynom(new double[] { 1,2,3}), 3, new Polynom(new double[] { 3,6,9}) },
+            new object[] {new Polynom(new double[] { 0}), 3, new Polynom(new double[] { }) },
+            new object[] {new Polynom(new double[] { 0,0,3}), 3, new Polynom(new double[] {0,0,9 }) }
+        };
+
+        static object[] toStringCases =
+        {
+            new object[] {new Polynom(new double[] {1,2,3 }), "1 * x^0 + 2 * x^1 + 3 * x^2" },
+            new object[] {new Polynom(new double[] {1 }), "1 * x^0" },
+            new object[] {new Polynom(new double[] { }), String.Empty }
+        };
+
+        static object[] getHashCodeCases =
+{
+            new object[] {new Polynom(new double[] { 1,2,3}), new Polynom(new double[] {1,2,3}), true },
+             new object[] {new Polynom(new double[] { 0,2,3}), new Polynom(new double[] {1,2,3}), false },
+             new object[] {new Polynom(new double[] { }), new Polynom(new double[] {0}), true }
+        };
+
+        static object[] equalsCases =
+{
+            new object[] {new Polynom(new double[] { 1,2,3}), new Polynom(new double[] {1,2,3}), true },
+             new object[] {new Polynom(new double[] { 0,2,3}), new Polynom(new double[] {1,2,3}), false },
+             new object[] {new Polynom(new double[] { }), new Polynom(new double[] {0}), true }
+        };
+
+        [Test, TestCaseSource(nameof(addCases))]
+        public void Polynoms_Add(Polynom poly1, Polynom poly2, Polynom result)
+        {
+            Assert.IsTrue((poly1 + poly2).ToString() == result.ToString());
+        }
+
+        [Test, TestCaseSource(nameof(multiplyCases))]
+        public void Polynoms_Multiply(Polynom poly1, Polynom poly2, Polynom result)
+        {
+            Assert.IsTrue((poly1 * poly2).ToString() == result.ToString());
+        }
+
+        [Test, TestCaseSource(nameof(subtractCases))]
+        public void Polynoms_Subtract(Polynom poly1, Polynom poly2, Polynom result)
+        {
+            Assert.IsTrue((poly1 - poly2).ToString() == result.ToString());
+        }
+
+        [Test, TestCaseSource(nameof(scalarMultiplyCases))]
+        public void Polynom_MultiplyScalar(Polynom poly1, double scalar, Polynom result)
+        {
+            Assert.IsTrue((poly1 * scalar).ToString() == result.ToString());
+        }
+
+        [Test, TestCaseSource(nameof(toStringCases))]
+        public void Polynom_MultiplyScalar(Polynom poly1, string result)
+        {
+            Assert.IsTrue(poly1.ToString() == result.ToString());
+        }
+
+        [Test, TestCaseSource(nameof(getHashCodeCases))]
+        public void Polynom_GetHashCode(Polynom poly1, Polynom poly2, bool result)
+        {
+            Assert.IsTrue((poly1.GetHashCode() == poly2.GetHashCode()) == result);
+        }
+
+        [Test, TestCaseSource(nameof(equalsCases))]
+        public void Polynom_Equals(Polynom poly1, Polynom poly2, bool result)
+        {
+            Assert.IsTrue(poly1.Equals(poly2) == result);
+        }
+    }
+}
---
+++ BSU.ASP.1501.Day3.Malchikov/Polynom/Polynom.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polynom
+{
+    public class Polynom
+    {
+        double[] Coefficients;
+
+        public int Length
+        {
+            get
+            {
+                return Coefficients.Length;
+            }
+        }
+
+        public Polynom(double[] coef)
+        {
+            Coefficients = coef;
+        }
+
+        public Polynom(double coef, int count)
+        {
+            Coefficients = new double[count];
+            for (int i = 0; i < count; i++)
+                Coefficients[i] = coef;
+        }
+
+        public Polynom(int count) : this(0, count) { }
+
+        public override string ToString()
+        {
+            string result = String.Empty;
+            string buf = String.Empty;
+            for (int i = 0; i < Coefficients.Length; i++)
+            {
+
+                if (Coefficients[i] != 0)
+                {
+                    buf = (i == 0) ? String.Empty : " + ";
+                    result += $"{buf}{Coefficients[i]} * x^{i}";
+                }
+            }
+            return result;
+        }
+
+        public override int GetHashCode()
+        {
+            return ToString().GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj.ToString() == ToString();
+        }
+
+        public double this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < Coefficients.Length)
+                return Coefficients[index];
+                throw new IndexOutOfRangeException();
+            }
+        }
+
+        public static Polynom operator*(Polynom poly, double coef)
+        {
+            double[] res = new double[poly.Length];
+
+            for (int i = 0; i < res.Length; i++)
+            {
+                res[i] = poly[i] * coef;
+            }
+
+            return new Polynom(res);
+        }
+
+        public static Polynom operator *(double coef, Polynom poly)
+        {
+            return poly * coef;
+        }
+
+        public static Polynom operator+(Polynom poly1, Polynom poly2)
+        {
+            int length = (int)Math.Max(poly1.Length, poly2.Length);
+
+            double[] res = new double[length];
+
+            for (int i = 0; i < length; i++)
+            { 
+                res[i] = poly1.GetCoef(i) + poly2.GetCoef(i);
+            }
+
+            return new Polynom(res);
+        }
+
+        public static Polynom operator-(Polynom poly1, Polynom poly2)
+        {
+            return poly1 + poly2 * (-1);
+        }
+
+        public static Polynom operator*(Polynom poly1, Polynom poly2)
+        {
+            Polynom result = new Polynom(poly1.Length + poly2.Length);
+            for (int i = 0; i < poly2.Length; i++)
+            {
+                double[] buf = new double[poly1.Length + i];
+                for (int j = 0; j < poly1.Length; j++)
+                {
+                    buf[j + i] = poly1[j] * poly2[i];
+                }
+                result += new Polynom(buf);
+            }
+
+            return result;
+        }
+
+        private double GetCoef(int index)
+        {
+            double param;
+
+            try { param = Coefficients[index]; }
+            catch { param = 0; }
+
+            return param;
+        }
+
+    }
+}
